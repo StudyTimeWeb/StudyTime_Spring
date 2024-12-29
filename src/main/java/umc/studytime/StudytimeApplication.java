9db00d0a588e656c3c07b2e0c5694a3eb532fca0
Jiyun <kjy5952@naver.com>
2024-12-29 10:40:21 +0900

    #1 Feat: first setting

---
+++ src/main/java/umc/studytime/StudytimeApplication.java
@@ -0,0 +1,13 @@
+package umc.studytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudytimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudytimeApplication.class, args);
+	}
+
+}
